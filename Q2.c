ae6de27f808eb8f6941014ad4feb197317836de6
muhammadanas20 <muhammadanasashfaq2006@gmail.com>
2025-09-17 11:36:14 +0500

    print point also in answer

--- Q2.c
+++ Q2.c
@@ -14,19 +14,19 @@ int main(){
     printf("Enter the x and y coordinates of the point (separated by space): ");
     scanf("%d %d", &x, &y);
     if(x == 0 && y == 0){
-        printf("The point lies on the origin, not in any quadrant.\n");
+        printf("The point (%d, %d) lies on the origin, not in any quadrant.\n", x, y);
     } else if(x == 0 && y != 0){
-        printf("The point lies on the y-axis, not in any quadrant.\n");
+        printf("The point (%d, %d) lies on the y-axis, not in any quadrant.\n", x, y);
     } else if(y == 0 && x != 0){
-        printf("The point lies on the x-axis, not in any quadrant.\n");
+        printf("The point (%d, %d) lies on the x-axis, not in any quadrant.\n", x, y);
     } else if(x > 0 && y > 0){
-        printf("The point lies in Quadrant I (+x, +y).\n");
+        printf("The point (%d, %d) lies in Quadrant I.\n", x, y);
     } else if(x < 0 && y > 0){
-        printf("The point lies in Quadrant II (-x, +y).\n");
+        printf("The point (%d, %d) lies in Quadrant II.\n", x, y);
     } else if(x < 0 && y < 0){
-        printf("The point lies in Quadrant III (-x, -y).\n");
+        printf("The point (%d, %d) lies in Quadrant III.\n", x, y);
     } else {
-        printf("The point lies in Quadrant IV (+x, -y).\n");
+        printf("The point (%d, %d) lies in Quadrant IV.\n", x, y);
     }
 
     return 0;
